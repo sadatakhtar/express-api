4e10930d2215597292af9ce40cb8b0e07ab269dd
sadat <shadowak47@yahoo.com>
2022-12-06 20:35:33 +0000

    Organised the routes via controllers

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-const express = require('express')
-const dotenv = require('dotenv')
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 // NB: Load env variables
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: './config/config.env' });
 
 const app = express()
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
-})
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}, running in ${process.env.NODE_ENV} mode`)
-})
+});
