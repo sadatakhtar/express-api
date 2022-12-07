c8b0825cf520de02e8ad9bbacfa98e399967d8c2
sadat <shadowak47@yahoo.com>
2022-12-07 10:44:38 +0000

    updated codebase, added logger

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const logger = require('./middleware/logger')
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
@@ -11,10 +12,9 @@ const app = express()
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
-
+app.use(logger);
 
 const port = process.env.PORT || 5000
-
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}, running in ${process.env.NODE_ENV} mode`)
